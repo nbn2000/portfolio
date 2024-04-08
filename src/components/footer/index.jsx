d6e76393288e5b3ec3b6ad81036363dcfa5f8563
nbn2000 <makhmudov4744@list.ru>
2024-04-08 14:00:01 +0500

    footer is ready

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,59 @@
+import { Link } from "react-router-dom";
+import {
+  WhiteLogoSmall,
+  Github,
+  Linkedin,
+  Twitter,
+  Telegram,
+} from "src/svg/view";
+
 const Footer = () => {
-  return <div>index</div>;
+  return (
+    <footer className="py-[32px] w-full border-t-[rgba(171,178,191,1)] border-solid border-t-[1px]">
+      <div className="container-x-axis  flex flex-col justify-center items-center gap-[48px]">
+        <div className="flex flex-col md:flex-row justify-center md:justify-between text-center md:items-start gap-[20px] flex-wrap md:flex-nowrap w-full">
+          <div className="flex flex-col justify-center md:justify-start items-center md:items-start gap-[16px]">
+            <div className="flex flex-row justify-center md:justify-start flex-wrap items-center md:items-start gap-[24px]">
+              <div className="flex flex-row justify-center items-center gap-[9px]">
+                <WhiteLogoSmall />
+                <span className="font-[500] text-[16px] leading-[20.99px]">
+                  Nodirbek
+                </span>
+              </div>
+              <span className="text-grey font-[400] text-[16px] leading-[20.99px]">
+                makhmudov.work4744@inbox.ru
+              </span>
+            </div>
+            <span className="font-[400] text-[16px] leading-[20.99px] ">
+              Back-end and front-end developer
+            </span>
+          </div>
+          <div className="flex flex-col justify-start  items-center gap-[12px]">
+            <span className="font-[500] text-[24px] leading-[31.49px]">
+              Media
+            </span>
+            <div className="flex flex-row justify-center items-center gap-[8px]">
+              <Link>
+                <Github />
+              </Link>
+              <Link>
+                <Linkedin />
+              </Link>
+              <Link>
+                <Twitter />
+              </Link>
+              <Link>
+                <Telegram />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <span className="font-[400] text-[16px] leading-[20.99px] text-grey">
+          © Copyright 2022. Made by Nodirbek
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/svg/view/index.js
+++ src/svg/view/index.js
@@ -1,6 +1,7 @@
 export { default as NotFoundIcon } from "../NotFoundIcon";
-export { default as DropDownIcon } from "../DropDownIcon";
 export { default as WhiteLogo } from "../WhiteLogo";
 export { default as Github } from "../Github";
 export { default as Twitter } from "../Twitter";
 export { default as Linkedin } from "../Linkedin";
+export { default as WhiteLogoSmall } from "../WhiteLogoSmall";
+export { default as Telegram } from "../Telegram";
