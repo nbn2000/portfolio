93c21781b3893eea74d3e39e4bc198a3972ffa89
nbn2000 <makhmudov4744@list.ru>
2024-07-12 17:17:38 +0500

    just checking test meta

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,14 @@ const About = () => {
     <>
       <Helmet>
         <title> Nodirbek | About </title>
+        <meta
+          name="description"
+          content="1111111Welcome to the portfolio of Nodirbek Makhmudov, a skilled front-end developer passionate about crafting beautiful and functional web experiences. Explore my projects and skills here."
+        />
+        <meta
+          property="og:description"
+          content="1111Welcome to the portfolio of Nodirbek Makhmudov, a skilled front-end developer passionate about crafting beautiful and functional web experiences. Explore my projects and skills here."
+        />
       </Helmet>
       <AboutView />
     </>
