8c3700123cbe16596eb5bd4e3836f674c7372ab9
nbn2000 <makhmudov4744@list.ru>
2024-07-12 17:34:33 +0500

    just change in meta test4

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,14 +6,6 @@ const About = () => {
     <>
       <Helmet>
         <title> Nodirbek | About </title>
-        <meta
-          name="description"
-          content="1111111Welcome to the portfolio of Nodirbek Makhmudov, a skilled front-end developer passionate about crafting beautiful and functional web experiences. Explore my projects and skills here."
-        />
-        <meta
-          property="og:description"
-          content="1111xWelcome to the portfolio of Nodirbek Makhmudov, a skilled front-end developer passionate about crafting beautiful and functional web experiences. Explore my projects and skills here."
-        />
       </Helmet>
       <AboutView />
     </>
