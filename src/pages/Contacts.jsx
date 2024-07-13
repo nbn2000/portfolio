87110c107b66f1e6ad46a793aa784bd1eb435beb
nbn2000 <makhmudov4744@list.ru>
2024-07-13 18:16:45 +0500

    test for meta 3

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 import ContactsView from "src/sections/contacts";
 
 const Contacts = () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 import HomeView from "src/sections/home";
 
 const Home = () => {
