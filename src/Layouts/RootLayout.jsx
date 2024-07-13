8b12fbf9aae0481a7c0fe6a4ecb3db13a00caf55
nbn2000 <makhmudov4744@list.ru>
2024-07-13 18:14:27 +0500

    test for meta 2

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,16 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import { HelmetProvider } from "react-helmet-async";
 import GoToTop from "./GoToTop";
 
 export default function RootLayout() {
   return (
-    <HelmetProvider>
+    <>
       <GoToTop />
       <Navbar />
       <Outlet />
       <Footer />
-    </HelmetProvider>
+    </>
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 import AboutView from "src/sections/about";
 
 const About = () => {
