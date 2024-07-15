4b4da78ce6d5368e0a8f1ee921045396751df403
nbn2000 <makhmudov4744@list.ru>
2024-07-15 11:10:59 +0500

    express 6

--- api/index.js
+++ api/index.js
@@ -3,17 +3,15 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 
-const PORT = 5000;
-
 // Define __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const filePath = path.resolve(__dirname, "dist", "index.html");
+const filePath = path.resolve(__dirname, "../dist", "index.html");
 
 const app = express();
 
 // Serve static files from the dist directory
-app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static(path.resolve(__dirname, "../dist")));
 
 // Handle root route and modify meta tags
 app.get("/", (req, res) => {
@@ -53,7 +51,26 @@ app.get("/about", (req, res) => {
   });
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// Fallback to handle all other routes
+app.get("*", (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    // Replace placeholders with default values
+    data = data
+      .replace(
+        /__TITLE__/g,
+        "Nodirbek Makhmudov - Front-End Developer Portfolio"
+      )
+      .replace(
+        /__DESCRIPTION__/g,
+        "Welcome to the portfolio of Nodirbek Makhmudov, a skilled front-end developer passionate about crafting beautiful and functional web experiences. Explore my projects and skills here."
+      );
+
+    res.send(data);
+  });
 });
+
+export default app;
