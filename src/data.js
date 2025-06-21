cf3e12836a24538052e83b35a4a7eab29aa5ae8d
nbn2000 <makhmudov4744@list.ru>
2025-06-21 22:56:45 +0500

    new profession added

--- src/data.js
+++ src/data.js
@@ -1,133 +1,134 @@
-
 export const skills = [
-    {
-      header: "Languages",
-      body: ["TypeScript,", "JavaScript"],
-    },
-    {
-      header: "Databases",
-      body: ["MongoDB,", "Firebase,", "MySQL,", "PostgreSQL"],
-    },
-    {
-      header: "Tools",
-      body: [
-        "VSCode,",
-        "WebStorm,",
-        "Kali Linux,",
-        "Windows,",
-        "Figma,",
-        "Git,",
-        "Github,",
-        "GitLab,",
-        "Jira,",
-      ],
-    },
-    {
-      header: "Frameworks",
-      body: [
-        "Tailwind CSS,",
-        "Expressjs,",
-        "Nextjs,",
-        "Bootstrap,",
-        "Nuxt,",
-        "Vuejs,",
-        "React Native,",
-        "Expo",
-      ],
-    },
-    {
-      header: "Libraries",
-      body: [
-        "React,",
-        "RTK Query,",
-        "Redux,",
-        "MaterialUI,",
-        "Styled Components,",
-        "Emotion,",
-        "Gsap,",
-        "NPM,",
-        "Tailwind CSS",
-      ],
-    },
-    {
-      header: "Other",
-      body: ["HTML,", "CSS,", "SCSS,", "REST,", "Wordpress,", "Tilda"],
-    },
-  ];
+  {
+    header: "Languages",
+    body: ["TypeScript,", "JavaScript"],
+  },
+  {
+    header: "Databases",
+    body: ["MongoDB,", "Firebase,", "MySQL,", "PostgreSQL"],
+  },
+  {
+    header: "Tools",
+    body: [
+      "VSCode,",
+      "WebStorm,",
+      "Kali Linux,",
+      "Windows,",
+      "Figma,",
+      "Git,",
+      "Github,",
+      "GitLab,",
+      "Jira,",
+    ],
+  },
+  {
+    header: "Frameworks",
+    body: [
+      "Tailwind CSS,",
+      "Expressjs,",
+      "Nextjs,",
+      "Bootstrap,",
+      "Nuxt,",
+      "Vuejs,",
+      "React Native,",
+      "Expo,",
+      "Electron.js,",
+      "Tauri",
+    ],
+  },
+  {
+    header: "Libraries",
+    body: [
+      "React,",
+      "RTK Query,",
+      "Redux,",
+      "MaterialUI,",
+      "Styled Components,",
+      "Emotion,",
+      "Gsap,",
+      "NPM,",
+      "Tailwind CSS",
+    ],
+  },
+  {
+    header: "Other",
+    body: ["HTML,", "CSS,", "SCSS,", "REST,", "Wordpress,", "Tilda"],
+  },
+];
 
-  export const projects = [
-    {
+export const projects = [
+  {
     name: "Oson Test",
-    link: "https://oson-test.uz/"
-    },
-{
-    name: 'Navbatda',
-    link: 'https://navbatda.uz/'
-    
-},
-{
-    name: 'Smart Office',
-    link: 'https://smart-office.uz/'
-    
-},
-{
-    name: 'Financial IT',
-    link: 'https://startups.financialit.net/'
-},
-{
+    link: "https://oson-test.uz/",
+  },
+  {
+    name: "Navbatda",
+    link: "https://navbatda.uz/",
+  },
+  {
+    name: "Smart Office",
+    link: "https://smart-office.uz/",
+  },
+  {
+    name: "Financial IT",
+    link: "https://startups.financialit.net/",
+  },
+  {
     name: "KPI",
-    link: "https://www.kpi.com/"
-},
-{
+    link: "https://www.kpi.com/",
+  },
+  {
     name: "Canalplus",
-    link: "https://www.canalplus.com/"
-},
-{
+    link: "https://www.canalplus.com/",
+  },
+  {
     name: "RahmatQr",
-    link: "https://rahmatqr.uz/"
-}
-]
+    link: "https://rahmatqr.uz/",
+  },
+  {
+    name: "GoodDrop",
+    link: "https://gooddrop.gg/",
+  },
+];
 
 export const aboutMyself = [
+  "Hello.",
+  "I’m Makhmudov Nodirbek — a full-stack developer with over 3 years of experience crafting seamless digital experiences across web and mobile applications.",
 
-"Hello.",
-"I’m Makhmudov Nodirbek — a full-stack developer with over 3 years of experience crafting seamless digital experiences across web and mobile applications.",
+  "I specialize in building robust, scalable, and user-friendly applications using technologies like ReactJS, NextJS, VueJS, NodeJS and React Native, among others. Over the years, I’ve contributed to projects for companies across Uzbekistan and Poland, including RealSoft Uzbekistan, DataSite Technologies and BlueSoft Poland.",
 
-"I specialize in building robust, scalable, and user-friendly applications using technologies like ReactJS, NextJS, VueJS, NodeJS and React Native, among others. Over the years, I’ve contributed to projects for companies across Uzbekistan and Poland, including RealSoft Uzbekistan, DataSite Technologies and BlueSoft Poland.",
+  "In my journey, I’ve been fortunate to collaborate with talented professionals and innovative teams, creating solutions that have left a lasting impact on users.",
 
-"In my journey, I’ve been fortunate to collaborate with talented professionals and innovative teams, creating solutions that have left a lasting impact on users.",
+  "Beyond coding, I work on personal projects, explore new technologies, and share insights with the developer community.",
 
-"Beyond coding, I work on personal projects, explore new technologies, and share insights with the developer community.",
+  "I’m always open to exciting opportunities. Feel free to reach out to me at makhmudov.work4744@inbox.ru.",
 
-"I’m always open to exciting opportunities. Feel free to reach out to me at makhmudov.work4744@inbox.ru.",
-
-"You can also find me on LinkedIn and GitHub."
-]
-
+  "You can also find me on LinkedIn and GitHub.",
+];
 
 export const socialLinks = [
-    {
-      name: "GitHub",
-      link: "https://github.com/nbn2000",
-    },
-    {
-      name: "LinkedIn",
-      link: "https://www.linkedin.com/in/nodirbek-makhmudov/",
-    },
-    {
-      name: "Telegram",
-      link: "https://t.me/Nodirbek4744",
-    },
-    {
-      name: "Hashnode",
-      link: "https://nodibek-makhmudov.hashnode.dev/",
-    },
-    {
-      name: "Email",
-      link: "mailto:makhmudov.work4744@inbox.ru",
-    },
-    {
-      name: "Twitter",
-      link: "https://x.com/Nodirbe77981460",
-    }
-  ];
+  {
+    name: "GitHub",
+    link: "https://github.com/nbn2000",
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/nodirbek-makhmudov/",
+  },
+  {
+    name: "Telegram",
+    link: "https://t.me/Nodirbek4744",
+  },
+  {
+    name: "Hashnode",
+    link: "https://nodibek-makhmudov.hashnode.dev/",
+  },
+  {
+    name: "Email",
+    link: "mailto:makhmudov.work4744@inbox.ru",
+  },
+  {
+    name: "Twitter",
+    link: "https://x.com/Nodirbe77981460",
+  },
+];
